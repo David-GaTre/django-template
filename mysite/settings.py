bb4ec8eb9a6b6b6e4bf94904127b2bdc0a138954
David Garcia <gtdavid2013@gmail.com>
2022-10-01 02:12:12 -0500

    Added default page

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!$-5+4sycxdeuxdvn=vf)3tz#pf@wai!yias-*!4-6a-f0qe&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "pages.apps.PagesConfig",
 ]
 
 MIDDLEWARE = [
