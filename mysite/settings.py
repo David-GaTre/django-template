d0a0603d57415edb30224c66da4c6b4443427e57
David Garcia <gtdavid2013@gmail.com>
2022-10-01 02:02:42 -0500

    Add default page

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!$-5+4sycxdeuxdvn=vf)3tz#pf@wai!yias-*!4-6a-f0qe&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
